27b4cd9d98f589a879c7c4386cf8fdf7ae189b62
uncleyong <1915992513@qq.com>
2018-05-17 12:32:56 +0800

    python操作redis，set并get值

--- db_fix/redis_operate.py
+++ db_fix/redis_operate.py
@@ -1,3 +1,7 @@
 import redis
-r = redis.Redis(host='127.0.0.1',port=6379,db=0)
-pass
+import json
+r = redis.Redis(host='127.0.0.1',port=6379,db=0,password='uncleyong@uncleyong')
+r.set('verification_code:register13811228811','{"validateCode":"111111","time":"1526523665998"}')
+res = json.loads(r.get('verification_code:register13811228811').decode('utf-8'))
+print(res,type(res))
+print(res['validateCode'])
